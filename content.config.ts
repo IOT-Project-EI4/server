71e40719fe58b33383beccac97d1e88c915a4dd4
Valentin Le Lievre <lelievre.valentin17@gmail.com>
2025-02-17 19:26:09 +0100

    Edited tree

--- content.config.ts
+++ content.config.ts
@@ -1,13 +1,17 @@
-import { defineCollection, defineContentConfig } from '@nuxt/content'
+import { defineCollection, defineContentConfig, z } from '@nuxt/content'
 
 export default defineContentConfig({
     collections: {
         test: defineCollection({
             // Load every file inside the `content` directory
             source: {
-                include: "**",
-                repository: "https://github.com/valentin-llv/CGS-server_Projet-EI4",
+                cwd: 'docs/resources-and-management',
+                include: '**',
             },
+
+            schema: z.object({
+                rawbody: z.string()
+            }),
 
             // Specify the type of content in this collection
             type: 'page',
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 export default defineNuxtConfig({
     compatibilityDate: '2025-02-01',
-    // devtools: { enabled: false },
+    devtools: { enabled: false },
 
     nitro: {
         experimental: {
